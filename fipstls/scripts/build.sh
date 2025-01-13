e177cba13906218859381b6b649f2164caa5d88e
Bobby Dhillon <bdhill@arista.com>
2025-01-13 23:43:53 +0000

    Update scripts/build.sh to exit on first failure

--- fipstls/scripts/build.sh
+++ fipstls/scripts/build.sh
@@ -1,46 +1,41 @@
 #!/usr/bin/env bash
 
-# This script runs tests for glibc 2.17 forward compatibility.
-# It builds and runs tests in Docker containers with glibc 2.17,
-# 2.31 (with openssl 1.1.1w), and 2.36 (with openssl 3.x).
-#
-# It also builds and runs tests on alpine 3.21 with openssl 3.x.
-
 cleanup() {
     echo "Caught cancellation signal. Terminating background processes..."
     pkill -P $$
     exit 1
 }
 
-# Trap SIGINT and SIGTERM signals
 trap cleanup SIGINT SIGTERM
 
 TAG=$(git rev-parse --short HEAD)
-TS=$(date +%s)
 DOCKERFILE="./scripts/Dockerfile"
-
-docker_build() {
-    local target=$1
-    docker build \
-        --target "$target" \
-        --progress=plain \
-        -t "$target":"$TAG" \
-        -f "$DOCKERFILE" .
-}
 
 start_time=$(date +%s)
 targets=("test-ossl3-alpine" "test-ossl1.1.1-glibc2.31" "test-ossl3-glibc2.36")
 pids=()
 
+# Run docker builds in parallel
 for target in "${targets[@]}"; do
-    docker_build "$target" 2>&1 | sed "s/^/[$target] /" &
+    (
+        docker build --target "$target" --progress=plain -t "$target":"$TAG" -f "$DOCKERFILE" . 2>&1 | sed "s/^/[$target] /"
+        exit ${PIPESTATUS[0]}
+    ) &
     pids+=($!)
 done
 
-set -e
-
-for pid in "${pids[@]}"; do
-    wait $pid || exit 1
+# Check builds in background
+while [ ${#pids[@]} -gt 0 ]; do
+    for pid in "${!pids[@]}"; do
+        if ! kill -0 ${pids[$pid]} 2>/dev/null; then
+            wait ${pids[$pid]} || {
+                pkill -P $$
+                exit 1
+            }
+            unset pids[$pid]
+        fi
+    done
+    sleep 0.1
 done
 
 end_time=$(date +%s)
