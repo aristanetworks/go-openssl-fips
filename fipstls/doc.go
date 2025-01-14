2788592afbba4d1fce1d6c67e9fb1de98a05a71a
Bobby Dhillon <bdhill@arista.com>
2025-01-14 19:36:03 +0000

    Update documentation

--- fipstls/doc.go
+++ fipstls/doc.go
@@ -2,19 +2,10 @@
 // When configured correctly, OpenSSL can be executed in FIPS mode, making the fipstls package
 // FIPS compliant.
 //
-// The [Context] must be configured before the client or [Dialer] can be used. The Context is
-// responsible for initializing libssl and the TLS [Config] that will be used to
-// create [Conn] connections.
-//
-// There are two options for initializing the [Context],
-//   - [NewCtx] will initialize the C.SSL_CTX with TLS configuration options only, but
-//     not create the underlying C.SSL_CTX object. Instead, the [Dialer] will create and
-//     cleanup the C.SSL_CTX every new [Conn] connection.
-//   - [NewUnsafeCtx] will both initialize and create the underlying C.SSL_CTX object once, and
-//     the [Dialer] will reuse it in creating multiple [Conn] connections.
-//
-// Creating the context once and reusing it is considered best practice by OpenSSL developers, as
-// internally to OpenSSL various items are shared between multiple SSL objects are cached in in
-// the C.SSL_CTX. The drawback is that the caller will be responsible for closing the context
-// which will cleanup the C memory allocated for it.
+// Prior to using any `fipstls` methods, the [`fipstls.Init`](https://pkg.go.dev/github.com/aristanetworks/go-openssl-fips/fipstls#Init) method must be called to dynamically load libssl. If that fails, then the program has the option to handle the error and fallback to default go/crypto.
+
+// There are three structs that the caller may use in creating TLS connections:
+// - The [`fipstls.Config`](https://pkg.go.dev/github.com/aristanetworks/go-openssl-fips/fipstls#Config) struct is used for configuring TLS options for the [`fipstls.Context`](https://pkg.go.dev/github.com/aristanetworks/go-openssl-fips/fipstls#Context).
+// - The [`fipstls.Dialer`](https://pkg.go.dev/github.com/aristanetworks/go-openssl-fips/fipstls#Dialer) creates a [`fipstls.Context`](https://pkg.go.dev/github.com/aristanetworks/go-openssl-fips/fipstls#Context) for every new [`fipstls.Conn`](https://pkg.go.dev/github.com/aristanetworks/go-openssl-fips/fipstls#Conn) connection. The [`fipstls.Conn`](https://pkg.go.dev/github.com/aristanetworks/go-openssl-fips/fipstls#Conn) connection is responsible for freeing the C memory allocated by OpenSSL when it is closed.
+// - The [`fipstls.Transport`](https://pkg.go.dev/github.com/aristanetworks/go-openssl-fips/fipstls#Transport) calls into the [`fipstls.Dialer`](https://pkg.go.dev/github.com/aristanetworks/go-openssl-fips/fipstls#Dialer) for creating a new TLS connection every roundtrip.
 package fipstls
